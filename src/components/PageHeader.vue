7e33f53b52fdd1dc413898fb1a9740c724a73c88
Wisdom-Daniel Efe <efewisdomd01@gmail.com>
2023-04-12 08:23:01 +0100

    Setup styles for project and Initialised Tailwind Setup

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { MoonIcon } from '@heroicons/vue/24/outline'
+// import { MoonIcon } from '@heroicons/vue/24/solid'
+</script>
+
+<template>
+    <div>
+        <h3>Where in the world?</h3>
+        <p><MoonIcon /> Dark Mode</p>
+    </div>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
   theme: {
+    fontFamily: {
+      sans: ['Nunito Sans', 'sans-serif']
+    },
+    colors: {
+      light: {
+        elements: '#FFFFFF',
+        background: '#FAFAFA',
+        text: '#111517',
+        input: '#858585'
+      },
+      dark: {
+        elements: '#2B3945',
+        background: '#202C37',
+        text: '#FFFFFF'
+      },
+      elements: {
+        light: '#FFFFFF',
+        dark: '#2B3945'
+      },
+      background: {
+        light: '#FAFAFA',
+        dark: '#202C37'
+      },
+      text: {
+        light: '#111517',
+        dark: '#FFFFFF'
+      }
+    },
     extend: {},
   },
   plugins: [],
